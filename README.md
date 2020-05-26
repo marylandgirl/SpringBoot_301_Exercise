1cc1199d47f7fe80de025fccdcc3bbd134242d20
Kim Levin <kbussie1@montgomerycollege.edu>
2020-05-25 21:08:34 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### This project is an implementation of exercise 3.02 from the book "Introduction to Java Spring Boot: Learning By Coding"
---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    @Min(1)
+    private int age;
+
+    public Dog() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
